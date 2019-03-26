2f95151f9fcb3607d9840229ede8941bf9103ee7
factory011 <13100721033@139.com>
2019-03-26 16:17:53 +0800

    Site updated: 2019-03-26 16:17:48

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Mar 26 2019 15:41:59 GMT+0800 (中国标准时间)
+// build time:Tue Mar 26 2019 16:17:36 GMT+0800 (中国标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
